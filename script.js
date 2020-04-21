bfeb05316c6181184aa0e19bab28b8d34ad0381c
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-04-21 14:14:56 +0300

    new day

--- script.js
+++ script.js
@@ -1,1 +1,123 @@
-console.log('hello world');
+// ********************** Variables and data types
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+*/
+
+/*
+// *********************Variable mutation and type coercion
+
+// Type coercion
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName); 
+*/
+
+// *****************Basic operators
+/*
+var now, yearJohn, yearJohn;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+//Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2)
+console.log(now * 2)
+console.log(now / 10)
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageMark);
+console.log(typeof 'Mark');
+var x;
+console.log(typeof x);
+*/
+
+// Operator precedence
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x , y);
+
+// More operators
+
+// x = x * 2;
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
+*/
+
+/*
+var johnMass, johnHeight, markMass, markHeight;
+
+markMass = 80;
+markHeight = 1.8;
+johnMass = 80;
+johnHeight = 1.9;
+
+markBMI = markMass / (markHeight * markHeight);
+console.log(markBMI);
+
+johnBMI = johnMass / (johnHeight * johnHeight);
+console.log(johnBMI);
+
+var BMI = markBMI > johnBMI;
+console.log(BMI);
+
+console.log('Is Mark`s BMI is higher than John`s? ' + BMI);
+*/
+
