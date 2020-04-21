7a18b4f34390fa76cf08fc78fd53b231a5083453
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-04-21 20:57:35 +0300

    first function

--- script.js
+++ script.js
@@ -268,3 +268,28 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 
 
 
+// **********************FUNCTIONS
+
+function calculateAge(birthYear) {
+  return 2018 - birthYear;
+  }
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+  if (retirement > 0) {
+  console.log(firstName + ' retires in ' + retirement + ' years.');
+  } else {
+    console.log(firstName + ' is already retired.');
+  }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1968, 'Jane');
