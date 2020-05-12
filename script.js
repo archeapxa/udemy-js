778045092d31ca31398e077b19dd4b760dd9675f
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-12 11:20:52 +0300

    lesson 66

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ var jane = Object.create(personProto, {
 
 // Primitives vs objects
 
-
+/*
 //primitives
 var a = 23;
 var b = a;
@@ -92,3 +92,42 @@ change(age, obj);
 
 console.log(age)
 console.log(obj.city)
+*/
+
+/////////////////
+// PASSING FUNCTIONS AS ARGUMENTS
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81){
+  return Math.round(206.9 - (0.76 * el));
+  } else {
+    return -1;
+  }
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages)
+console.log(fullAges)
+console.log(rates)
