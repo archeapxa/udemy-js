14b0f3ea067c744fe4109ccda5ba2c61e8626a42
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-12 13:11:34 +0300

    68 CLOSURES

--- script.js
+++ script.js
@@ -171,6 +171,8 @@ function game () {
 
 game();
 */
+
+/*
 (function (goodLuck) {
   var score = Math.random() * 10;
   console.log(score >= 5)
@@ -180,3 +182,60 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck)
 })(5);
+*/
+
+// CLOSURES
+/*
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+
+
+//retirement(66)(1990);
+
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what is UX?')
+    }
+  } else if ( job === 'teacher') {
+    return function (name) {
+      console.log(name + ', what subject do you teach?')
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what is UX?')
+    } else if ( job === 'teacher') {
+      console.log(name + ', what subject do you teach?')
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion('teacher')('John')
+*/
+
