56acb8f404b961791699bdedcb167bf53b6149fe
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-13 12:25:34 +0300

    71 challengt

--- script.js
+++ script.js
@@ -240,7 +240,7 @@ interviewQuestion('teacher')('John')
 */
 
 // BIND CALL APPLY
-
+/*
 var john = {
   name: 'John',
   age: 26,
@@ -298,3 +298,46 @@ var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
+*/
+
+(function() {
+
+function Question(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function() {
+  console.log(this.question);
+
+  for(var i = 0; i < this.answers.length; i++) {
+    console.log(i + ': ' + this.answers[i]);
+  }
+}
+
+Question.prototype.checkAnswer = function(ans) {
+  if (ans === this.correct) {
+    console.log('Correct answer!')
+  } else {
+    console.log('Wrong answer! Try it again')
+  }
+}
+
+var q1 = new Question('Is JavaScripts the coolest programmin language?', ['Yes', 'No'], 0);
+
+var q2 = new Question('What`s the name of this course`s teacher?', ['John', 'Michael', 'Jonas'], 2);
+
+var q3 = new Question('What does best describe coding??', ['Boring', 'Hard', 'Fun', 'Tidious'], 2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select correct answer.'));
+
+questions[n].checkAnswer(answer);
+
+})();
