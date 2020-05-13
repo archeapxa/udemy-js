47ccba9ba23c611156a83aebbfb1a2f1997ad50a
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-13 13:56:11 +0300

    end of section

--- script.js
+++ script.js
@@ -299,7 +299,7 @@ var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
 */
-
+/*
 (function() {
 
 function Question(question, answers, correct) {
@@ -341,3 +341,77 @@ var answer = parseInt(prompt('Please select correct answer.'));
 questions[n].checkAnswer(answer);
 
 })();
+*/
+
+
+(function() {
+
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+  
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+  
+    for(var i = 0; i < this.answers.length; i++) {
+      console.log(i + ': ' + this.answers[i]);
+    }
+  }
+  
+  Question.prototype.checkAnswer = function(ans, callback) {
+    if (ans === this.correct) {
+      var sc;
+
+      console.log('Correct answer!');
+      sc = callback(true);
+    } else {
+      console.log('Wrong answer! Try it again')
+
+      sc = callback(false);
+    }
+    this.displayScore(sc);
+  }
+  
+  Question.prototype.displayScore = function (score) {
+    console.log('Your currnt score is: ' + score);
+    console.log('-------------------------');
+  }
+
+  var q1 = new Question('Is JavaScripts the coolest programmin language?', ['Yes', 'No'], 0);
+  
+  var q2 = new Question('What`s the name of this course`s teacher?', ['John', 'Michael', 'Jonas'], 2);
+  
+  var q3 = new Question('What does best describe coding??', ['Boring', 'Hard', 'Fun', 'Tidious'], 2);
+  
+  var questions = [q1, q2, q3];
+
+  function score() {
+    var sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++;
+      } 
+      return sc;
+    }
+  }
+  var keepScore = score();
+
+  function nextQuestion() {
+
+    var n = Math.floor(Math.random() * questions.length);
+    
+    questions[n].displayQuestion();
+    
+    var answer = prompt('Please select correct answer.');
+    
+    if(answer !== 'exit') {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
+
+  })();
