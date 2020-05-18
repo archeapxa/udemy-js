c53061646391da244a88469ab2ebeef5a1bde806
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-18 20:39:47 +0300

    78 end

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var ctrlAddItem = function() {
+    
+    // 1. Get the filed input data
 
+    // 2. add the item to the budget controller
+
+    // 3. add the new item to UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+    console.log('lalala');
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
