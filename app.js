e665ec701f675e497b7fb537baeb1d722392dff6
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-19 18:05:35 +0300

    84 end

--- app.js
+++ app.js
@@ -121,6 +121,20 @@ var UIController = (function() {
 
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     },
@@ -150,6 +164,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the new item to UI
     UICtrl.addListItem(newItem, input.type);
+    // 4. Clear the fields
+    UICtrl.clearFields();
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
