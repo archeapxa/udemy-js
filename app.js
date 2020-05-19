c5fa653d98c1a8b92a1f033c95249ebc01451c6f
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-19 13:17:58 +0300

    79 end

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-  //some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,  // wiil be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
   
 })();
 
@@ -17,10 +36,13 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     
     // 1. Get the filed input data
-
+    var input = UICtrl.getInput();
+    console.log(input)
     // 2. add the item to the budget controller
 
     // 3. add the new item to UI
@@ -28,10 +50,9 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-    console.log('lalala');
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
