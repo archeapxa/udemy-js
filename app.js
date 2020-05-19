d957538bac56cb5c784ee65b5418169fdaa62c93
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-19 13:32:06 +0300

    80 end

--- app.js
+++ app.js
@@ -36,13 +36,20 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   var ctrlAddItem = function() {
     
     // 1. Get the filed input data
     var input = UICtrl.getInput();
-    console.log(input)
     // 2. add the item to the budget controller
 
     // 3. add the new item to UI
@@ -50,14 +57,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
+  };
+
+  return {
+    init: function() {
+      console.log('start');
+      setupEventListeners();
+    }
   }
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
-
 })(budgetController, UIController);
+
+controller.init();
