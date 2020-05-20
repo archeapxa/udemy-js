167c381b5006d68354f80cb719298250bce964f8
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-20 16:12:21 +0300

    87end

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var budgetController = (function() {
     },
     totals: {
       exp: 0,
-      inx: 0
+      inc: 0
     },
     budget: 0,
     percentage: -1,
@@ -71,7 +71,7 @@ var budgetController = (function() {
       data.budget = data.totals.inc - data.totals.exp 
 
       // calculate the percent of inc
-      if (data.totalInc > 0) {
+      if (data.totals.inc > 0) {
       data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100)
       } else { data.percentage = -1 }
     },
@@ -102,6 +102,10 @@ var UIController = (function() {
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
     expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabeL: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage',
   };
 
   return {
@@ -168,6 +172,20 @@ var UIController = (function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabeL).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      
+      
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     },
@@ -198,7 +216,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var budget = budgetCtrl.getBudget();
     
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   }
 
   var ctrlAddItem = function() {
@@ -225,6 +243,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     init: function() {
       console.log('start');
       setupEventListeners();
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
     }
   }
 
