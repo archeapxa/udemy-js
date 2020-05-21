626c3dcfb8cbabe6a049901d081bffdba769a18b
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-21 20:12:39 +0300

    end 96

--- app.js
+++ app.js
@@ -145,7 +145,8 @@ var UIController = (function() {
     incomeLabeL: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage',
   };
 
   return {
@@ -231,6 +232,24 @@ var UIController = (function() {
 
     },
 
+    displayPercentages: function(percentages) {
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+      });
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     },
@@ -274,7 +293,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 2. Read perc from the budget controller
     var percentages = budgetCtrl.getPercentages();
     // 3. Update UI with new perc
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
 
   };
 
