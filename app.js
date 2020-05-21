d9d9604cbf64cf6f5f944441e6b44c6288d0e81f
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-21 17:04:56 +0300

    92 end

--- app.js
+++ app.js
@@ -168,7 +168,12 @@ var UIController = (function() {
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
 
+    deleteListItem: function(selectorID) {
+
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
 
     },
 
@@ -269,8 +274,9 @@ var controller = (function(budgetCtrl, UICtrl) {
       // 1. Delete the item from the data sctricture
       budgetCtrl.deleteItem(type, ID);
       // 2. Delete the item from the UI
-
+      UICtrl.deleteListItem(itemID);
       // 3. Update and show the new budget
+      updateBudget();
     }
 
   };
