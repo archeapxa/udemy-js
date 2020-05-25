98153ca16e686234d8f69af48f917785800c2bd0
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-25 16:45:25 +0300

    113 end

--- script.js
+++ script.js
@@ -285,4 +285,78 @@ Array.from(all).forEach(cur => cur.style)
 */
 
 // REST PARAMETERS
+/*
+// ES5
+function isFullAge5() {
+  var argsArray = Array.prototype.slice.call(arguments);
+  console.log(argsArray);
 
+  argsArray.forEach(function(cur) {
+    console.log((2016 - cur) >= 18)
+  });
+}
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6 (...years) {
+  years.forEach(cur => console.log((2016 - cur) >= 18));
+} 
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
+
+/*
+function isFullAge5(limit) {
+  var argsArray = Array.prototype.slice.call(arguments, 1);
+  console.log(argsArray);
+
+  argsArray.forEach(function(cur) {
+    console.log((2016 - cur) >= limit)
+  });
+}
+//isFullAge5(21, 1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+
+//ES6
+function isFullAge6 (limit, ...years) {
+  years.forEach(cur => console.log((2016 - cur) >= limit));
+} 
+isFullAge6(12, 1990, 1999, 1965, 2016, 1987);
+*/
+
+// DEFAULT PARAMETERS
+
+//ES5
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+  lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+  nationality === undefined ? nationality = 'american' : nationality = nationality;
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+
+var john = new SmithPerson('John', 1990)
+console.log(john);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
+console.log(emily)
+*/
+
+
+//ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+
+
+var john = new SmithPerson('John', 1990)
+console.log(john);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
+console.log(emily)
