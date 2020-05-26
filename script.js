b311dbebbf57f182bc11b7501a236d960cecda77
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-26 15:47:14 +0300

    116 end

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ ages6 = years.map((el, index) => {
 
 console.log(ages6);
 */
-
+/*
 //ES 5
 var box5 = {
   color: 'green',
@@ -127,7 +127,7 @@ const box6 = {
   }
 }
 box6.clickMe();
-
+*/
 /*
 //ES 6
 const box66 = {
@@ -397,7 +397,7 @@ console.log(question.get(ans === question.get('correct')));
 */
 
 // CLASSES
-
+/*
 //ES5
 
 var Person5 = function(name, yearOfBirth, job) {
@@ -434,3 +434,66 @@ const john6 = new Person6('John', 1990, 'teacher');
 john6.calculateAge();
 
 Person6.greeting();
+*/
+
+//ES5
+
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age)
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job);
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+}
+Athlete5.prototype = Object.create(Person5.prototype);
+
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals)
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+
+//ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals)
+  }
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10)
+
+johnAthlete6.calculateAge()
+johnAthlete6.wonMedal();
