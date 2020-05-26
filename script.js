7910659297ad5da8c638b85d7e2c7663b9b80a77
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-26 13:49:39 +0300

    114 end

--- script.js
+++ script.js
@@ -346,7 +346,7 @@ var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
 console.log(emily)
 */
 
-
+/*
 //ES6
 function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
   this.firstName = firstName;
@@ -360,3 +360,37 @@ var john = new SmithPerson('John', 1990)
 console.log(john);
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
 console.log(emily)
+*/
+
+
+// LECTURE: MAPS
+
+const question = new Map();
+question.set('question', 'What is the official name of the latets major JS version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong,  try again');
+
+question.get('question')
+console.log( question.get('question'));
+// console.log(question.size);
+
+if (question.has(4)) {
+//  question.delete(4);
+}
+//question.clear();
+
+//question.forEach((value, key) => console.log(`This is ${key}, and its set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+};
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
