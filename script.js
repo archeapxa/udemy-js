282c61b7942e0c17534e025aa3f96a4231232df3
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-26 15:22:47 +0300

    115 end

--- script.js
+++ script.js
@@ -364,7 +364,7 @@ console.log(emily)
 
 
 // LECTURE: MAPS
-
+/*
 const question = new Map();
 question.set('question', 'What is the official name of the latets major JS version?');
 question.set(1, 'ES5');
@@ -394,3 +394,43 @@ for (let [key, value] of question.entries()) {
 
 const ans = parseInt(prompt('Write the correct answer'));
 console.log(question.get(ans === question.get('correct')));
+*/
+
+// CLASSES
+
+//ES5
+
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age)
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+john5.calculateAge();
+
+//ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+  static greeting() {
+    console.log('Hey there!');
+  }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+john6.calculateAge();
+
+Person6.greeting();
