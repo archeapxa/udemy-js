ac1a7433c9240cf0533e36a51e6f6e0ba44e58b3
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-06-19 15:51:36 +0300

    140 end, api

--- forkify-9/src/js/index.js
+++ forkify-9/src/js/index.js
@@ -1,4 +1,17 @@
 // Global app controller
+//const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+//https://forkify-api.herokuapp.com/api/search
 
+import axios from 'axios';
 
+async function getResults(query) {
+  try {
 
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error)
+  }
+}
+getResults(`pasta`);
