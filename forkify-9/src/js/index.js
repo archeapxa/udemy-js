ce3dae4a2c98304ae728e6e1183769dce2cb2fc9
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-06-19 14:42:04 +0300

    start

--- forkify-9/src/js/index.js
+++ forkify-9/src/js/index.js
@@ -1,9 +1,4 @@
 // Global app controller
-import string  from './models/Search';
-//import { add as a, multiply, ID } from './views/searchView';
 
-import * as searchView from './views/searchView';
-
-console.log(`Using imported functions ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 2)}. ${string}.`);
 
 
