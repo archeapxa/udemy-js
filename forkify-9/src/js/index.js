69be9d1806f181c831cfe49dbd21d263e96818a6
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-06-20 21:36:05 +0300

    150 end

--- forkify-9/src/js/index.js
+++ forkify-9/src/js/index.js
@@ -5,6 +5,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app 
@@ -69,6 +70,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // prepare UI for changes
+    recipeView.cleanRecipe();
+    renderLoader(elements.recipe);
 
     // create new recipe object
     state.recipe = new Recipe(id);
@@ -85,7 +88,8 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
   
       // render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
 
     } catch (error) {
       alert('Error processing recipe!');
